072ca2c0bd0c11cc572dcd5ecc8a83a640e6e9b9
ruan <contatoruanfs@gmail.com>
2026-08-28 00:13:14 -0300

    fix: pacients now work with two or more names.

--- mylib.c
+++ mylib.c
@@ -3,50 +3,57 @@
 #include <stdlib.h>
 #include "mylib.h"
 
-void add_paciente(VetPacientes *pacientes) {
-	FILE *file;
-	char nome[64];
-	char contato[64];
-	char choice;
-
-	printf("Digite o nome do paciente: ");
-	getchar();
-	fgets(nome,64, stdin);
-	nome[strcspn(nome, "\n")] = '\0';
-	printf("Digite o e-mail do paciente: ");
-	fgets(contato,64, stdin);
-	contato[strcspn(contato, "\n")] = '\0';
-	printf("Adicionar o paciente %s, contato: %s com o id: %d? (y/n)\n", nome, contato, pacientes->qtd);
-	scanf(" %c", &choice);
-
-	if(choice == 'n') {
-		return;
+void add_paciente(VetPacientes *pacientes){
+    FILE *file;
+    char nome[64];char contato[64]; char choice;
+    
+    printf("Digite o nome do paciente: ");
+    getchar();
+    fgets(nome,64, stdin);
+    nome[strcspn(nome, "\n")] = '\0';
+    printf("Digite o e-mail do paciente: ");
+    fgets(contato,64, stdin);
+    contato[strcspn(contato, "\n")] = '\0';
+    
+    	
+	int maior = 0;
+	if(pacientes->qtd > 0){
+	    for(int i = 0; i<pacientes->qtd; i++){
+	        if(pacientes->itens[i].id > maior){maior = pacientes->itens[i].id;}
+	    }
 	}
-	printf("Adicionando...\n\n");
-
+	
+    
+    printf("Adicionar o paciente %s, contato: %s com o id: %d? (y/n)\n", nome, contato, maior+1);
+    scanf(" %c", &choice);
+    
+    if(choice == 'n'){return;}
+    printf("Adicionando...\n\n");
+    
 
 	file = fopen("pacientes.txt","a"); //a-append
 
-	if(file == NULL) {
-		printf("Erro ao abrir arquivo");
-		return;
+	if(file == NULL){
+	    printf("Erro ao abrir arquivo");
+	    return;
 	}
 
-	//fprintf é uma função para escrever no arquivo.
-	fprintf(file, "%s %s %d\n", nome, contato, pacientes->qtd);
-
+	//fprintf é uma função para escrever no arquivo. 
+	fprintf(file, "%s | %s | %d\n", nome, contato, maior+1);
+	
 	fclose(file);
-
+	
 	strcpy(pacientes->itens[pacientes->qtd].nome, nome);
 	strcpy(pacientes->itens[pacientes->qtd].contato, contato);
-	pacientes->itens[pacientes->qtd].id = pacientes->qtd;
-
+	pacientes->itens[pacientes->qtd].id = maior+1;
+	
 	pacientes->qtd++;
-
+	
 	printf("Paciente cadastrado.\n");
-
+	
 
 }
+
 void search_paciente(VetPacientes *pacientes) {
 	int id;
 	if(pacientes->qtd == 0) {
@@ -62,17 +69,18 @@ void search_paciente(VetPacientes *pacientes) {
 	printf("id: %d nome: %s contato: %s\n", pacientes->itens[id].id, pacientes->itens[id].nome, pacientes->itens[id].contato);
 }
 
-void read_pacientes(VetPacientes *pacientes) {
-	FILE *file;
-
-	int i = 0;
+void read_pacientes(VetPacientes *pacientes){
+    FILE *file;
+    
+    int i = 0;
 	file = fopen("pacientes.txt", "r");
 
 	if(file == NULL){
 	    return;
 	}
+	while(fscanf(file, "%63[^|] | %s | %d\n", &pacientes->itens[i].nome, &pacientes->itens[i].contato, &pacientes->itens[i].id) != EOF){i++;}
 	pacientes->qtd = i;
-
+	
 	fclose(file);
 }
 
